3e07f5a6818e346f24102837d180d9f3e8fc1f7b
aoao2002 <12011014@mail.sustech.edu.cn>
2023-04-06 22:36:46 +0800

    update some service

--- src/main/java/com/example/funding/service/Application/ApplicationService.java
+++ src/main/java/com/example/funding/service/Application/ApplicationService.java
@@ -1,5 +1,8 @@
 package com.example.funding.service.Application;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface ApplicationService {
 
 }
--- src/main/java/com/example/funding/service/Expenditure/ExpenditureService.java
+++ src/main/java/com/example/funding/service/Expenditure/ExpenditureService.java
@@ -1,5 +1,8 @@
 package com.example.funding.service.Expenditure;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface ExpenditureService {
 
 }
