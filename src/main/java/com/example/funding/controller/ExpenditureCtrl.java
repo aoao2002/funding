e62d4f8f384cb913208fbe69dcb2a83ce1d55127
aoao2002 <12011014@mail.sustech.edu.cn>
2023-04-07 17:07:27 +0800

    update some api frame

--- src/main/java/com/example/funding/controller/ExpenditureCtrl.java
+++ src/main/java/com/example/funding/controller/ExpenditureCtrl.java
@@ -5,13 +5,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Set;
+
 @RestController
 @RequestMapping("/expenditure/")
 public class ExpenditureCtrl {
     // 展示经费使用情况 还有很多种展示方式
-    @RequestMapping(value ="edit/showExpenditure", method= RequestMethod.POST)
+    @RequestMapping(value ="view/showExpenditure", method= RequestMethod.POST)
     @ResponseBody
-    public boolean showExpenditure(){
+    public boolean showExpenditure(Set<String> groups_name, Set<String> expenditure_name){
         // TODO
         return false;
     }
