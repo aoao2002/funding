a4445448ac0c611d48e4e047986cdc05358758fb
俞向阳 <1343022453@qq.com>
2023-05-11 19:15:41 +0800

    可运行

--- src/test/java/com/example/funding/controller/TestExpenditure.java
+++ src/test/java/com/example/funding/controller/TestExpenditure.java
@@ -1,6 +1,13 @@
 package com.example.funding.controller;
 
+import com.example.funding.service.Application.ApplicationService;
+import net.bytebuddy.utility.RandomString;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.text.ParseException;
 
 public class TestExpenditure {
     /**
@@ -10,10 +17,34 @@ public class TestExpenditure {
      * 2. groupName 不属于这个人的组
      * 3. 时间不对
      *
-     * Test:插入随机信息
+     * SaResult newExpenditureApplication(String expenditureName, String groupName, String expenditureNumber,
+     *                                               double expenditureTotalAmount, String beginTime, String endTime)
+     *
+     * Test:插入随机信息，对于组别都有
      */
+    @Autowired
+    ApplicationCtrl applicationCtrl;
+    @Autowired
+    ApplicationService applicationService;
+
+
+//    参演：group-imed/1, mana-aoao/4, staff-y/1
+    String groupName = "imed";
+    String managerName = "aoao";
+    String staffName = "y";
+
     @Test
-    public void testExpend(){
+    public void testExpend() throws ParseException {
+        double amount = 10000;
+        String beginTime = "2022-02-22 12:00:00", endTime = "2023-02-02 12:11:12";
+//        先插入一个合法的，
+        String expName = "national_nature", expNumber = RandomStringUtils.random(10);
+        long res = applicationService.newExpenditureApplication(expName, groupName, expNumber,
+                amount, beginTime, endTime, 1L);
+        Assertions.assertTrue(res > -1);
+
+
+
 
     }
 }
