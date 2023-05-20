41d3fe2a0d672913d031844cc56a2fb3d6a02c6f
aoao2002 <12011014@mail.sustech.edu.cn>
2023-05-20 21:22:58 +0800

    update some test api

---
+++ src/test/java/com/example/funding/service/Expenditure/ExpenditureServiceMplTest.java
@@ -0,0 +1,34 @@
+package com.example.funding.service.Expenditure;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExpenditureServiceMplTest {
+
+    @Test
+    void getOneExpenditureAllInfo() {
+
+    }
+
+    @Test
+    void updateExpenditureQuota() {
+
+    }
+
+    @Test
+    void updateExpenditureEndTime() {
+    }
+
+    @Test
+    void getAllExpenditureInfoInOneGroup() {
+    }
+
+    @Test
+    void getAllExpenditureInfo() {
+    }
+
+    @Test
+    void getOneExpenditureAppAllInfoByMyself() {
+    }
+}
